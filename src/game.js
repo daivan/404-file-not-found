09596ce6c0764c98931c6d6c8cfa1dcba72b177f
Daivan Trinh <daivan.trinh@xpeedio.se>
2019-08-14 16:19:20 +0200

    Added an enemy

--- src/game.js
+++ src/game.js
@@ -13,13 +13,24 @@ var enemy = kontra.sprite({
 		y:80,
 		width:16,
 		height: 16,
-		color: 'red'
+		color: 'red',
+		direction: 'left'
 });
+
+
 
 var cooldown=0;
 var steps=[];
 var loop = kontra.gameLoop({
 update: function(){
+
+if(enemy.x==0){
+	enemy.direction='right'
+}
+
+if(enemy.x==240){
+	enemy.direction='left'
+}
 
 	if(cooldown>15){
 
@@ -28,21 +39,42 @@ update: function(){
 			sprite.y-=16;
 			cooldown=0;
 			steps.push('up');
+			if(enemy.direction=='left'){
+				enemy.x-=16;
+			}else{
+				enemy.x+=16;
+			}
+			
 		}
 		if(kontra.keys.pressed('down')){
 			sprite.y+=16;
 			cooldown=0;
 			steps.push('down');
+			if(enemy.direction=='left'){
+				enemy.x-=16;
+			}else{
+				enemy.x+=16;
+			}
 		}
 		if(kontra.keys.pressed('left')){
 			sprite.x-=16;
 			cooldown=0;
 			steps.push('left');
+			if(enemy.direction=='left'){
+				enemy.x-=16;
+			}else{
+				enemy.x+=16;
+			}
 		}
 		if(kontra.keys.pressed('right')){
 			sprite.x+=16;
 			cooldown=0;
 			steps.push('right');
+			if(enemy.direction=='left'){
+				enemy.x-=16;
+			}else{
+				enemy.x+=16;
+			}
 		}
 
 	}
